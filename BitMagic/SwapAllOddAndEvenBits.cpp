1050fd7f67a312bfc8a9f6f4113f1b8da141196b
Tushar Mohod <tusharmohod10@gmail.com>
2022-03-26 23:37:57 +0530

    changes in swap all odd and even bits prog

--- BitMagic/SwapAllOddAndEvenBits.cpp
+++ BitMagic/SwapAllOddAndEvenBits.cpp
@@ -1,15 +1,5 @@
 unsigned int swapAllOddAndEvenBits(unsigned int n) {
-	unsigned int temp = n;
-	unsigned int f = 3;
-	while(n > 0) {
-		if(((n & 1) == 1) && (((n >> 1) & 1) == 0)) {
-			temp = (temp ^ f);
-		}
-		else if(((n & 1) == 0) && (((n >> 1) & 1) == 1)) {
-			temp = (temp ^ f);
-		}
-		f = f << 2;
-		n = n >> 2;
-	}
-	return temp;
+	unsigned int o=0x55555555, e=0xaaaaaaaa;
+	unsigned int op=o&n, ep=e&n;
+	return (op<<1|ep>>1);
 }
