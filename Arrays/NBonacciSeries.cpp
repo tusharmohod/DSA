d2bafb6c652b2dd4b5c45c1c961ab2570477d67d
Tushar Mohod <tusharmohod10@gmail.com>
2022-04-02 01:09:31 +0530

    added nbonacci series in arrays

---
+++ Arrays/NBonacciSeries.cpp
@@ -0,0 +1,13 @@
+void bonacciseries(long n, int m) { 
+	int a[m] = { 0 }; 
+	a[n - 1] = 1; 
+	a[n] = 1; 
+
+	for (int i = n + 1; i < m; i++) {
+		a[i] = 2 * a[i - 1] - a[i - n - 1]; 
+    }
+
+	for (int i = 0; i < m; i++) {
+		cout << a[i] << " "; 
+    }
+} 
