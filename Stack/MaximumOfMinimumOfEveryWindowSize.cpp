320daca36b74d07a358814b58101bb154dea4296
Tushar Mohod <tusharmohod10@gmail.com>
2022-05-25 19:52:26 +0530

    modified max min of every wind stack for 0 to n

--- Stack/MaximumOfMinimumOfEveryWindowSize.cpp
+++ Stack/MaximumOfMinimumOfEveryWindowSize.cpp
@@ -1,15 +1,12 @@
 vector <int> maximumOfMinimumOfEveryWindowSize(int arr[], int n) {
-    vector<int> res(n + 1, 0), ans(n);
-    vector<int> pse(n + 1), nse(n + 1);
+    vector<int> res(n, 0);
+    vector<int> pse(n, -1), nse(n, n);
     stack<int> s;
-    pse[0] = -1;
-    nse[n] = n;
     for(int i = 0; i < n; i++) {
         while(!s.empty() && arr[s.top()] >= arr[i]) {
             s.pop();
         }
-        int ans = s.empty() ? -1 : s.top();
-        pse[i] = ans;
+        if(!s.empty()) pse[i] = s.top();
         s.push(i);
     }
     
@@ -21,23 +18,18 @@ vector <int> maximumOfMinimumOfEveryWindowSize(int arr[], int n) {
         while(!s.empty() && arr[s.top()] >= arr[i]) {
             s.pop();
         }
-        int ans = s.empty() ? n : s.top();
-        nse[i] = ans;
+        if(!s.empty()) nse[i] = s.top();
         s.push(i);
     }
     
     for(int i = 0; i < n; i++) {
-        int len = nse[i] - pse[i] - 1;
+        int len = nse[i] - pse[i] - 1 - 1;
         res[len] = max(res[len], arr[i]);
     }
     
-    for(int i = n - 1; i >= 1; i--) {
+    for(int i = n - 2; i >= 0; i--) {
         res[i] = max(res[i], res[i + 1]);
     }
     
-    for(int i = 1; i <= n; i++) {
-        ans[i - 1] = res[i];
-    }
-    
-    return ans;
+    return res;
 }
