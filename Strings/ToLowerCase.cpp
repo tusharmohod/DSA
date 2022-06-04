abd1d2b2a7ed13b47a779824e3f5e99d913eefdd
Tushar Mohod <tusharmohod10@gmail.com>
2022-06-04 23:20:29 +0530

    added to lower case

---
+++ Strings/ToLowerCase.cpp
@@ -0,0 +1,8 @@
+string toLowerCase(string s) {
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] = 'a' + (s[i] - 'A');
+        }
+    }
+    return s;    
+}
