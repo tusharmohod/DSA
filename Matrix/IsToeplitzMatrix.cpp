2da442c3514d4e444d840c67bc348917f6b6e609
Tushar Mohod <tusharmohod10@gmail.com>
2022-06-19 12:38:03 +0530

    added 2 matrix 2 strings

---
+++ Matrix/IsToeplitzMatrix.cpp
@@ -0,0 +1,11 @@
+bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    for(int i = 1; i < m; i++) {
+        for(int j = 1; j < matrix[i].size(); j++) {
+            if(matrix[i][j] != matrix[i - 1][j - 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
